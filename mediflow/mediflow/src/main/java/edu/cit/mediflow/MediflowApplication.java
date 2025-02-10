8930a1f6883a006921f40d3d9f8ac2a5b5a6166f
VincentPaul434 <shadowvator@gmail.com>
2025-02-10 08:50:45 +0800

    Add files via upload

---
+++ mediflow/mediflow/src/main/java/edu/cit/mediflow/MediflowApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.mediflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediflowApplication.class, args);
+	}
+
+}
---
+++ mediflow/mediflow/src/test/java/edu/cit/mediflow/MediflowApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.mediflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
