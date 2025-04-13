b665bbf8598d5fb3469b7fc65a3f0a32ebd10448
paxx <85264198+asherpaquit@users.noreply.github.com>
2025-04-13 19:03:01 +0800

    salamat lord

--- mediflow/mediflow/src/main/java/edu/cit/mediflow/controller/UserPatientController.java
+++ mediflow/mediflow/src/main/java/edu/cit/mediflow/controller/UserPatientController.java
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/user-patients")
-@CrossOrigin
 public class UserPatientController {
 
     @Autowired
